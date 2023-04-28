186507b8ab454670a56058175c142f6dd765bee8
TimofeyBM <borodin2505@gmail.com>
2023-04-29 00:01:04 +0300

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = "Private key"
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/Api key",
+      chainId: 11155111,
+      accounts: [
+        PRIVATE_KEY
+      ],
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/Api key",
+      chainId: 5,
+      accounts: [
+        PRIVATE_KEY
+      ],
+    },
+    main: {
+      url: "https://eth-mainnet.g.alchemy.com/v2/Api key",
+      chainId: 1,
+      accounts: [
+        PRIVATE_KEY
+      ],
+    },
+    polygon: {
+      url: "https://polygon-mainnet.g.alchemy.com/v2/Api key",
+      chainId: 137,
+      accounts: [
+        PRIVATE_KEY
+      ],
+    },
+  },
 };
